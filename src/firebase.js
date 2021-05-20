bdf125dbf773ac6cb41867cbd185d296eb9fbf30
박도희 <dohee.pa@gmail.com>
2021-05-20 23:44:23 +0900

    1.1 Securing the Keys

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_IN,
+  appId: process.env.REACT_APP_ID,
+};
+
+export default firebase.initializeApp(firebaseConfig);
