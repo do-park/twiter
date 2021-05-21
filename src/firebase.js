a545db551989bd2acdea18a0717ba23ffb8a68fb
박도희 <dohee.pa@gmail.com>
2021-05-22 01:48:43 +0900

    1.2 Router Setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase/app";
+import firebase from 'firebase';
+// import firebase from "firebase/app";
+// import * as firebase from 'firebase';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
