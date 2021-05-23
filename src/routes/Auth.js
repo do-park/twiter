367f8ee0d98082f3da445115268102e880680c2b
박도희 <dohee.pa@gmail.com>
2021-05-23 12:08:29 +0900

    2.5 Social Login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from 'fbase';
+import { authService, firebaseInstance } from 'fbase';
 import React, { useState } from 'react';
 
 const Auth = () => {
@@ -23,12 +23,23 @@ const Auth = () => {
         await authService.signInWithEmailAndPassword(email, password);
       }
     } catch (error) {
-      console.log(error.message);
       setError(error.message);
     }
-
-  }
+  };
   const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === 'google') {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === 'github') {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -39,8 +50,8 @@ const Auth = () => {
       {error}
       <span onClick={toggleAccount}>{newAccount ? 'Sign In' : 'Create Account'}</span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">Continue with Google</button>
+        <button onClick={onSocialClick} name="github">Continue with Github</button>
       </div>
     </div>)
 };
